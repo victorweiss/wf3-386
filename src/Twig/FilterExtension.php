ecea74c4e5b80282e3c6eb2356fdc69cfd812fd1
Victor Weiss <bonjour@victor-weiss.fr>
2020-05-27 10:49:34 +0200

    Début création extension Twig blogItem

---
+++ src/Twig/FilterExtension.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class FilterExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
+            new TwigFilter('prix', [$this, 'makePrix']),
+        ];
+    }
+
+    public function makePrix($prix, $sign = '€')
+    {
+        return number_format($prix,2, ',', ' ') . ' '.$sign;
+    }
+}
