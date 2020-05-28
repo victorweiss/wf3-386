f2018a7a9e19610fe94ee53b99eea67f723fd9ef
Victor Weiss <bonjour@victor-weiss.fr>
2020-05-28 16:58:04 +0200

    EmailService redirect en DEV

--- src/Service/EmailService.php
+++ src/Service/EmailService.php
@@ -8,18 +8,30 @@ use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 class EmailService {
 
     private $mailer;
+    private $MY_EMAIL;
+    private $APP_ENV;
 
     // Injection de service
     // On injecte MailerInterface dans notre classe EmailService
     // pour qu'il soit accessible dans toutes les méthodes de la classe
-    public function __construct(MailerInterface $mailer) {
+    public function __construct(
+        $MY_EMAIL,
+        $APP_ENV,
+        MailerInterface $mailer
+    ) {
+        $this->MY_EMAIL = $MY_EMAIL;
+        $this->APP_ENV = $APP_ENV;
         $this->mailer = $mailer;
     }
 
     public function sendEmail($data) {
+
+        $to = $data['to'] ?? $this->MY_EMAIL;
+        if ($this->APP_ENV == 'dev') { $to = $this->MY_EMAIL; } // Redirection DEV
+
         $email = (new TemplatedEmail())
-            ->from($data['from'] ?? 'demo.wf3.victor@gmail.com')
-            ->to($data['to'] ?? 'demo.wf3.victor@gmail.com')
+            ->from($data['from'] ?? $this->MY_EMAIL)
+            ->to($to)
             ->subject($data['subject'])
             ->htmlTemplate($data['template'])
             ->context($data['context'])
